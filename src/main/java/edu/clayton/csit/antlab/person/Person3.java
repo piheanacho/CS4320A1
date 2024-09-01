193077d92a22bb2d2e4fe98df2cc70f4b683e472
Precious Iheanacho <piheanacho100@gmail.com>
2024-08-31 20:25:34 -0400

    added method to Person3 files

--- src/main/java/edu/clayton/csit/antlab/person/Person3.java
+++ src/main/java/edu/clayton/csit/antlab/person/Person3.java
@@ -42,7 +42,16 @@ public class Person3 {
 	 * @return the reversed string
 	 */
 	private String calc(String input) {
-	  //Person 3 put your implementation here
-	  return null;
+		//Person 3 put your implementation here
+		String originalStr = "gtg123b";
+
+        // convert String to character array
+        // by using toCharArray
+        char[] try1 = originalStr.toCharArray();
+
+        for (int i = try1.length - 1; i >= 0; i--)
+            System.out.print(try1[i]);
+
+	  	return null;
 	}
 }
